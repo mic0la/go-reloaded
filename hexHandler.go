fc7c3edd55ff1a2efd7bc0c8ba95ba771bcf6429
mtoktasy <mtoktasy@L02R04S05s-iMac.local>
2024-02-19 18:13:46 +0600

    dasda

--- hexHandler.go
+++ hexHandler.go
@@ -1,7 +1,5 @@
 package cadet
 
-import "fmt"
-
 func CheckChar(v byte) bool {
 	if (v >= '0' && v <= '9') || (v >= 'A' && v <= 'F') {
 		return true
@@ -13,8 +11,11 @@ func HexHandler(s []byte) []byte {
 	for i, v := range s {
 		if CheckChar(v) && CheckChar(s[i+1]) {
 			if string(s[i+2:i+8]) == " (hex)" {
-				fmt.Println("reached")
-				return []byte{55}
+				// fmt.Println("reached")
+				// return []byte{55}
+
+				s[i : i+8] = "edited value"
+				return s
 			}
 		}
 	}
